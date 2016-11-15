cb3af07d214816df6c86edb8d24e6d568ce4397d
Nelma <nelmasb@gmail.com>
2016-11-15 09:41:56 -0200

    alterado forma de tratar errro

--- Excecoes/src/Principal.java
+++ Excecoes/src/Principal.java
@@ -1,17 +1,23 @@
 
 public class Principal {
 
-	public static void main(String[] args) throws Exception {
-		a(100);
+	public static void main(String[] args) {
+		a(-100);
+		System.out.println("termianndo execução de main");
 	}
 
-	public static void a(int i) throws Exception {
-		System.out.println("executando a() com "+ 1);
-		b(i);
+	public static void a(int i){
+		System.out.println("executando a() com "+ i);
+		try {
+			b(i);
+		} catch (Exception e) {
+			System.out.println("tratando a exceção:  "+ e.getMessage());
+		}
+		System.out.println("termianndo execução de a");
 	}
 	
 	public static void b(int b) throws Exception {
-		System.out.println("executando b() com "+ 1);
+		System.out.println("executando b() com "+ b);
 		throw new Exception("mensagem");
 	}
 }
